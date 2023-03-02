e5d13b1ed6eafbe4be8845036b18a4279ed740f3
Gautham742 <gauthamk742@gmail.com>
2023-03-02 11:40:44 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { DistrictComponent } from './district/district.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    DistrictComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/assets/data.ts
@@ -0,0 +1,73 @@
+export const districts = [
+    {
+        name: "Thiruvananthapuram",
+        desc: "Thiruvananthapuram (or Trivandrum) is the capital of the southern Indian state of Kerala. . ",
+        Image:"https://keralatravels.com/userfiles/1474446260__1341.jpeg",
+    },
+    {
+        name: "Kollam",
+        desc: "Kollam is a city in the state of Kerala, on India's Malabar Coast. It’s known as a trade hub and for its beaches. . ",
+        Image:"https://www.adameshandbook.com/wp-content/uploads/2020/01/jatayu-1.jpg",
+    },
+    {
+        name: "Pathanamthitta",
+        desc: "Pathanamthitta, is a municipality situated in the Central Travancore region in the state of Kerala ",
+        Image:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/17/fb/80/perunthenaruvi-waterfall.jpg?w=700&h=-1&s=1",
+    },
+    {
+        name: "Alappuzha",
+        desc: "Alappuzha (or Alleppey) is a city on the Laccadive Sea in the southern Indian state of Kerala. . ",
+        Image:"https://s3.india.com/travel/wp-content/uploads/2017/05/Alleppey.jpg",
+    },
+    {
+        name: "Kottayam",
+        desc: "Kottayam is a municipal town in the Indian state of Kerala.",
+        Image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Vembanad_Lake_at_Kumarakom.jpg/1920px-Vembanad_Lake_at_Kumarakom.jpg",
+    },
+    {
+        name: "Ernakulam",
+        desc: "Sprawling, residential Ernakulam is known for Marine Drive, a busy waterfront promenade.  . ",
+        Image:"https://spb.kerala.gov.in/sites/default/files/styles/inner_banner_image_850x460/public/2020-09/4_1.jpg?itok=QdkvH5Ma",
+    },
+    {
+        name: "Palakkad",
+        desc: "Palakkad, or Palghat, is a city in Kerala, a state in southwestern India. ",
+        Image:"https://spb.kerala.gov.in/sites/default/files/styles/inner_banner_image_850x460/public/2020-09/palakkad.jpg?itok=Xd75QKHG",
+    },
+    {
+        name: "Kasaragod",
+        desc: "Kasaragod is a municipal town and administrative headquarters of Kasaragod district in the state of Kerala ",
+        Image:"https://www.keralatourism.org/images/destination/mobile/aquatourism_in_kasaragod20131107114603_306_1.jpg",
+    },
+    {
+        name: "Idukki",
+        desc: "Idukki is a township in Idukki district near to the district headquarter Painavu in the state of Kerala ",
+        Image:"https://keralatourism.travel/images/destinations/headers/idukki-kerala-tourism-entry-fee-timings-holidays-reviews-header.jpg",
+    },
+    {
+        name: "Thrissur",
+        desc: "Thrissur is a city in the south Indian state of Kerala ",
+        Image:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Kudamatom_at_thrissur_pooram_2013_7618.JPG/1920px-Kudamatom_at_thrissur_pooram_2013_7618.JPG",
+    },
+
+    {
+        name: "Malappuram",
+        desc: "Malappuram is a city in the Indian state of Kerala, spread over an area of 58.20 km² ",
+        Image:"https://www.keralatourism.org/images/enchanting_kerala/large/thirunavaya_malappuram20230105140626_1260_1.jpg",
+    },
+    {
+        name: "Wayanad",
+        desc: "Wayanad is a rural district in Kerala state, southwest India. In the east, the Wayanad Wildlife Sanctuary is a lush ",
+        Image:"https://img.onmanorama.com/content/dam/mm/en/travel/kerala/images/2021/10/21/wayanad.jpg",
+    },
+    {
+        name: "Kozhikode",
+        desc: "Kozhikode is a coastal city in the south Indian state of Kerala. It was a significant spice trade center ",
+        Image:"https://www.keralatourism.org/images/destination/mobile/kozhikode_beach20150811120557_256_1.jpg",
+    },
+    {
+        name: "Kannur",
+        desc: "Kannur is a coastal city in the south Indian state of Kerala. ",
+        Image:"https://www.dtpckannur.com/public/img/inner/kannur.jpg",
+    },
+]
